f78750722b66addc12ea8e352d690cebc600ee34
lohikaarme <stewart.watt@gmail.com>
2022-07-30 11:36:41 -0700

    Experimenting with rdbg

--- bin/ruby_script.rb
+++ bin/ruby_script.rb
@@ -14,16 +14,16 @@
 # isogram?("Odin")
 
 
-require 'pry-byebug'
+# require 'pry-byebug'
 
 def yell_greeting(string)
     name = string
 
-    binding.pry
+    # binding.pry
 
     name = name.upcase
     greeting = "WASSAP, #{name}!"
     puts greeting
 end
 
-yell_greeting()
+yell_greeting('test')
