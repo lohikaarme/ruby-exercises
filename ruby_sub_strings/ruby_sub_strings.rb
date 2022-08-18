4872f322a2da2ea80af523349c1232e24140c7e5
lohikaarme <stewart.watt@gmail.com>
2022-08-17 20:56:33 -0700

    Initial setup of substrings exercise

---
+++ ruby_sub_strings/ruby_sub_strings.rb
@@ -0,0 +1,6 @@
+# Ruby sub-string exercise to create a method #substring that will take an
+# an argument and substrings, will return a hash listing and count
+
+def substring("string", dictionary)
+
+end
