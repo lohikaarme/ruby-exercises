907f38ff83f1b4cbd8cfc06efd3cc2e982b269ba
lohikaarme <stewart.watt@gmail.com>
2022-08-17 20:43:41 -0700

    Added description and default value

--- caesar_cipher/caesar_cipher.rb
+++ caesar_cipher/caesar_cipher.rb
@@ -1,4 +1,7 @@
-def caesar_cipher(string, shift)
+# caesar_cipher exercise to create a cipher 
+# that will shift characters by a set value
+
+def caesar_cipher(string, shift = 0)
     alpha = ("a".."z").to_a
     ciphered = string.split("").map do |value| 
         char_check = alpha.find_index(value)
@@ -15,3 +18,5 @@ end
 
 shifted = caesar_cipher("This is a test of the method 'caesar_cipher'",5)
 caesar_cipher(shifted,-5)
+
+caesar_cipher("This is another test of the method 'caesar_cipher'")
