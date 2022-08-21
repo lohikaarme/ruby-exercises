79d14b8fa5fcf7ced0ae690af96daf4b8e29a3a2
lohikaarme <stewart.watt@gmail.com>
2022-08-20 22:46:36 -0700

    Will find all dictionary substrings included in string

--- ruby_sub_strings/ruby_sub_strings.rb
+++ ruby_sub_strings/ruby_sub_strings.rb
@@ -1,6 +1,14 @@
 # Ruby sub-string exercise to create a method #substring that will take an
 # an argument and substrings, will return a hash listing and count
 
-def substring("string", dictionary)
+dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
+test_string = "Howdy partner, sit down! How's it going?"
+
+def substring(string, dictionary)
+    dictionary.each do |word|
+        p word if string.include?(word)
+    end
 
 end
+
+substring(test_string, dictionary)
