970ba841faeda51c25deafea4fdbf36112eded1e
lohikaarme <stewart.watt@gmail.com>
2022-09-10 22:20:02 -0700

    Added #legal_move checker

--- tic_tac_toe/game.rb
+++ tic_tac_toe/game.rb
@@ -17,12 +17,21 @@ class Game
 
   def update_board(input)
     location_mapper(input)
-    @board[@row][@column] = 'X'
+    if legal_move 
+      @board[@row][@column] = 'X'
+      p @board[@row][@column]
+    else 
+      p 'Illegal move!'   
+    end
   end
 
   def location_mapper(location)
     @row = (location/3.0).ceil - 1
     @column = location - (@row * 3) - 1
+  end
+
+  def legal_move
+    @board[@row][@column].nil?
   end
 
 end
--- tic_tac_toe/player.rb
+++ tic_tac_toe/player.rb
@@ -1,29 +1,43 @@
 # frozen_string_literal: true
 
 # player actions and inputs
-
 class Player
   attr_accessor :move
   attr_reader :p1, :p2, :p1_sym, :p2_sym
-  
+
   def initialize(p1_name = 'P1', p2_name = 'P2')
     @p1 = p1_name
     @p2 = p2_name
     @p1_sym = 'X'
     @p2_sym = 'O'
+    @turn = @p1
+    @turn_num = 0
   end
-  
+
   def player_move
     i = true
     while i
-      p "it is your turn, place your symbol (0-9):"
+      p "#{@turn}, it is your turn, place your symbol (0-9):"
       begin
         @move = Kernel.gets.match(/\d{1}/)[0].to_i
       rescue StandardError
         puts 'Erroneous input! Try again...'
       else
         i = false
+        turn_changer
       end
     end
+  end
+
+  def turn_changer
+    @turn_num =
+      if @turn_num.zero
+        1
+      else
+        0
+      end
+    players = [@p1, @p2]
+    @turn = players[@turn_num]
+    p @turn_num
   end
 end
