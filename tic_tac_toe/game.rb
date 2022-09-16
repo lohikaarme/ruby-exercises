b9e88581b71e36dbc3d37207d18303d2507f578c
lohikaarme <stewart.watt@gmail.com>
2022-09-15 22:45:20 -0700

    Added #diagonal_checker win logic

--- tic_tac_toe/game.rb
+++ tic_tac_toe/game.rb
@@ -38,13 +38,22 @@ class Game
   def win_checker(player)
     row_checker(@board, player)
     column_checker(@board, player)
+    diagonal_checker(@board, player)
   end
 
   def row_checker(board, player)
-    board.each { |row| puts "Player Wins" if row[0] == player && row.uniq.size == 1 }
+    board.each { |row| puts "#{player[:name]} Wins" if row[0] == player && row.uniq.size == 1 }
   end
 
   def column_checker(board, player)
     row_checker(board.transpose, player)
+  end
+
+  def diagonal_checker(board, player)
+    slash = [
+      [board[0][0], board[1][1], board[2][2]],
+      [board[2][0], board[1][1], board[0][2]]
+    ]
+    row_checker(slash, player)
   end
 end
